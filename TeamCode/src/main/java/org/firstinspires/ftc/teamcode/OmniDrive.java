086a8db2216367f7b890ec97c90c7a6a997d5fbc
Morgan <ideacreeper@gmail.com>
2018-10-10 18:43:10 -0400

    OmniDrive.java added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDrive.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import java.lang.Math;
+
+public class OmniDrive {
+
+    private DcMotor wheel1, wheel2, wheel3;
+    private HardwareMap hardwareMap;
+    private Gamepad gamepad1;
+    private Gamepad gamepad2;
+    private Telemetry telemetry;
+
+    public OmniDrive(HardwareMap hMap, Gamepad gamepad1, Gamepad gamepad2, Telemetry telemetry) {
+        hardwareMap = hMap;
+        this.gamepad1 = gamepad1;
+        this.gamepad2 = gamepad2;
+        this.telemetry = telemetry;
+        wheel1 = (DcMotor) hardwareMap.get("m1");
+        wheel2 = (DcMotor) hardwareMap.get("m2");
+        wheel3 = (DcMotor) hardwareMap.get("m3");
+    
+        driveCode(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);    
+
+    public void driveCode(double xRaw, double yRaw, double rRaw, int speedAug = 1, boolean Logic = true){
+        if (this.Logic && gamepad1.left_bumper){
+            if(this.speedAug >= 1) { //I changed it, happy?
+                this.speedAug -= 1;
+            }
+            this.Logic = false;
+        }
+        else if(this.Logic && gamepad1.right_bumper) {
+            this.speedAug += 1;
+            this.Logic = false;
+        }
+        else if(!gamepad1.left_bumper && !gamepad1.right_bumper){
+            this.Logic = true;
+        }
+
+        x = xRaw / this.speedAug;
+        y = yRaw / this.speedAug;
+        r = rRaw / this.speedAug;
+       
+        //Trig motor stuff
+        wheel1.setPower(x*Math.cos(-Math.PI)-y*Math.sin(-Math.PI) + r);
+        wheel2.setPower(x*Math.cos(Math.PI/3)-y*Math.sin(Math.PI/3) + r);
+        wheel3.setPower(x*Math.cos(-Math.PI/3)-y*Math.sin(-Math.PI/3) + r);
+
+    }
+
+    public void telemetry(){
+        //Motors
+        telemetry.addData("Motor 1:", " %.2f", (wheel1.getPower());
+        telemetry.addData("Motor 2:", " %.2f", (wheel2.getPower());
+        telemetry.addData("Motor 3:", " %.2f\n", (wheel3.getPower());
+        //Speed Modifier
+        telemetry.addData("Mod:", " %d", (speedAug));
+        //Sticks
+        telemetry.addData("Left Stick:", "(%.2f, %.2f) ", (gamepad1.left_stick_x, gamepad1.right_stick_x));
+        telemetry.addData("Right Stick:", "(%.2f, %.2f) ", (gamepad1.left_stick_x, gamepad1.left_stick_x));
+    }
+}
