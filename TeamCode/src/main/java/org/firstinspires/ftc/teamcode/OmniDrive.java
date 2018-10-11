7c9bc9c3b1672c965152e8bbda4f360cf1046480
Morgan <ideacreeper@gmail.com>
2018-10-10 20:13:54 -0400

    Made OmniDrive.java work... java is hard when you don't look at docs!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniDrive.java
@@ -19,39 +19,47 @@ public class OmniDrive {
     private Gamepad gamepad1;
     private Gamepad gamepad2;
     private Telemetry telemetry;
+    private boolean logic;
+    private int speedAug;
 
     @Override
     public void init(){
         wheel1 = (DcMotor) hardwareMap.get("m1");
         wheel2 = (DcMotor) hardwareMap.get("m2");
         wheel3 = (DcMotor) hardwareMap.get("m3");
+        logic = true;
+        speedAug = 1;
     }
     
     @Override
     public void loop(){
+<<<<<<< HEAD
         driveCode(gamepad1.left_stick_x, gamepad1.left_stick_y, (gamepad1.right_stick_x + gamepad1.right_trigger - gamepad1.left_trigger));
+=======
+        driveCode(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x, logic);
+>>>>>>> Made OmniDrive.java work... java is hard when you don't look at docs!
         telemetry();
     }           
 
-    public void driveCode(double xRaw, double yRaw, double rRaw, int speedAug = 1, boolean Logic = true){
-        if (this.Logic && gamepad1.left_bumper){
-            if(this.speedAug >= 1) { //I changed it, happy?
-                this.speedAug -= 1;
+    public void driveCode(double xRaw, double yRaw, double rRaw, boolean driveLogic){
+        x = xRaw / speedAug;
+        y = yRaw / speedAug;
+        r = rRaw / speedAug;
+
+        if (driveLogic&& gamepad1.left_bumper){
+            if(speedAug >= 1) { //I changed it, happy?
+                speedAug -= 1;
             }
-            this.Logic = false;
+            driveLogic= false;
         }
-        else if(this.Logic && gamepad1.right_bumper) {
-            this.speedAug += 1;
-            this.Logic = false;
+        else if(driveLogic&& gamepad1.right_bumper) {
+            speedAug += 1;
+            driveLogic= false;
         }
         else if(!gamepad1.left_bumper && !gamepad1.right_bumper){
-            this.Logic = true;
+            driveLogic= true;
         }
 
-        x = xRaw / this.speedAug;
-        y = yRaw / this.speedAug;
-        r = rRaw / this.speedAug;
-       
         //Trig motor stuff
         wheel1.setPower(x*Math.cos(-Math.PI)-y*Math.sin(-Math.PI) + r);
         wheel2.setPower(x*Math.cos(Math.PI/3)-y*Math.sin(Math.PI/3) + r);
@@ -61,13 +69,19 @@ public class OmniDrive {
 
     public void telemetry(){
         //Motors
+<<<<<<< HEAD
         telemetry.addData("Motor 1:", " %.2f", (wheel1.getPower());
         telemetry.addData("Motor 2:", " %.2f", (wheel2.getPower());
         telemetry.addData("Motor 3:", " %.2f", (wheel3.getPower());
+=======
+        telemetry.addData("Motor 1:", " %.2f", (wheel1.getPower()));
+        telemetry.addData("Motor 2:", " %.2f", (wheel2.getPower()));
+        telemetry.addData("Motor 3:", " %.2f\n", (wheel3.getPower()));
+>>>>>>> Made OmniDrive.java work... java is hard when you don't look at docs!
         //Speed Modifier
         telemetry.addData("Mod:", " %d\n", (speedAug));
         //Sticks
-        telemetry.addData("Left Stick:", "(%.2f, %.2f) ", (gamepad1.left_stick_x, gamepad1.right_stick_x));
-        telemetry.addData("Right Stick:", "(%.2f, %.2f) ", (gamepad1.left_stick_x, gamepad1.left_stick_x));
+        telemetry.addData("Left Stick:", "(%.2f, %.2f) ", gamepad1.left_stick_x, gamepad1.right_stick_x);
+        telemetry.addData("Right Stick:", "(%.2f, %.2f) ", gamepad1.left_stick_x, gamepad1.left_stick_x);
     }
 }
