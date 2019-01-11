a9dbdd768f25ec9ec3dff988c816a64bdd8d83ec
potsdamhighschool <jheuser742@potsdam.k12.ny.us>
2019-01-11 18:29:16 -0500

    LAST MINUTE CHANGES eee

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousStuff.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousStuff.java
@@ -18,18 +18,75 @@ public class AutonomousStuff extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         hardware = new BetterTankHardware(hardwareMap);
         //int red = robot.getColorSensor().red();
-        //synchronized (this) {
-        hardware.armSeg2.setPower(.75);
-        sleep(1000);
+
+        //Set arm to get robot down
+        hardware.armSeg2.setPower(.375);
+        sleep(2000);
         hardware.armSeg1.setPower(0);
         sleep(500);
+
+        //Turn Away to dislodge hook
+        hardware.setDriveMotors(-.2,.2);
+        hardware.armSeg2.setPower(-.375);
+        sleep(750);
+        hardware.setDriveMotors(0,0);
+        sleep(2000);
+        hardware.armSeg1.setPower(0);
+
+        //Go forward Away from center
+        hardware.setDriveMotors(.5,.5);
+        sleep(500);
+        hardware.setDriveMotors(0,0);
+        sleep(500);
+
+        //Turn Back
         hardware.setDriveMotors(.5,-.5);
         sleep(1000);
-        hardware.setDriveMotors(50,50);
-        sleep(1000);
-        hardware.setDriveMotors(-.5,.5);
-        sleep(1000);
         hardware.setDriveMotors(0,0);
-        //}
+
+        int highestColor = 0;
+        if(hardware.colorSensor.green() > hardware.colorSensor.blue()){
+            highestColor = hardware.colorSensor.green();
+        }
+        else{
+            highestColor = hardware.colorSensor.blue();
+        }
+
+        if(hardware.colorSensor.red() > highestColor + 150){
+            //This object is Yellow
+            hardware.setDriveMotors(.25,.25);
+            sleep(1000);
+            hardware.setDriveMotors(0,0);
+        }
+        else {
+            //This object is white(?)
+            hardware.setDriveMotors(.2,-.2);
+            sleep(750);
+            hardware.setDriveMotors(0,0);
+
+            if(hardware.colorSensor.green() > hardware.colorSensor.blue()){
+                highestColor = hardware.colorSensor.green();
+            }
+            else{
+                highestColor = hardware.colorSensor.blue();
+            }
+
+            //Try the other one, maybe it's yellow?
+            if(hardware.colorSensor.red() > highestColor + 150){
+                hardware.setDriveMotors(.25,.25);
+                sleep(1000);
+                hardware.setDriveMotors(0,0);
+            }
+            else{
+                //Out of objects, just go to the last one
+
+                hardware.setDriveMotors(-.2,.2);
+                sleep(1500);
+                hardware.setDriveMotors(0,0);
+                hardware.setDriveMotors(.25,.25);
+                sleep(1000);
+                hardware.setDriveMotors(0,0);
+            }
+        }
     }
 }
